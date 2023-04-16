2d4232f9fd1db061a8ec2505de66041c5e2d4db1
BlueEel <baptiste.chasset@gmail.coml>
2023-04-16 22:22:00 +0200

    add quest composer 3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '80961424c4b0ef416f2f8788f9e99c2eb81d5a92',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '80961424c4b0ef416f2f8788f9e99c2eb81d5a92',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'gipetto/cowsay' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '85218ee4c0d18cc8753e08af54a8740aac12860a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gipetto/cowsay',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
